0e56a27ba70504a9e05bc05fbc763af93f1619d4
Kellerman Rivero <riverokellerman@gmail.com>
2024-01-07 23:45:59 +0100

    [Chapter3] Make Y axis depend on X axis

--- chapter-3/chapter3/GraphPaperView.swift
+++ chapter-3/chapter3/GraphPaperView.swift
@@ -27,16 +27,25 @@ struct GraphPaperView: View {
         .background(.white)
     }
     
+    func numberOfLines(size: CGSize) -> (x: Int, y: Int) {
+        let x = numberOfLines
+        let aspectRatio = size.width / size.height
+        let y = Int(CGFloat(numberOfLines) / aspectRatio )
+        return (x, y)
+    }
+    
     func scaleFactor(size: CGSize) -> (x: CGFloat, y: CGFloat) {
-        let x = (size.width) / CGFloat(numberOfLines)
-        let y = (size.height) / CGFloat(numberOfLines)
+        let (xLines, yLines) = numberOfLines(size: size)
+        let x = (size.width) / CGFloat(xLines)
+        let y = (size.height) / CGFloat(yLines)
         return (x, y)
     }
     
     func origin(size: CGSize) -> (x: CGFloat, y: CGFloat) {
+        let (xLines, yLines) = numberOfLines(size: size)
         let (scaleX, scaleY) = scaleFactor(size: size)
-        let x = scaleX * (CGFloat(numberOfLines) / 2)
-        let y = scaleY * (CGFloat(numberOfLines) / 2)
+        let x = scaleX * (CGFloat(xLines) / 2)
+        let y = scaleY * (CGFloat(yLines) / 2)
         return (x, y)
     }
     
@@ -46,15 +55,20 @@ struct GraphPaperView: View {
     }
     
     func drawSquares(context: GraphicsContext, size: CGSize) {
+        let (xLines, yLines) = numberOfLines(size: size)
         let transform = scaleTransform(size: size)
-        for line in 0...numberOfLines {
+        for line in 0...xLines {
             let s = CGFloat(line)
             var path = Path()
             path.move(to: CGPoint(x: s, y: 0))
             path.addLine(to: CGPoint(x: s, y: size.width))
             path = path.applying(transform)
             context.stroke(path, with: .color(.gray), lineWidth: 0.2)
-            
+        }
+        
+        for line in 0...yLines {
+            let s = CGFloat(line)
+            var path = Path()
             path = Path()
             path.move(to: CGPoint(x: 0, y: s))
             path.addLine(to: CGPoint(x: size.height, y: s))
@@ -65,6 +79,8 @@ struct GraphPaperView: View {
     
     func drawXAxis(context: GraphicsContext, size: CGSize) {
         let (_, yOrigin) = origin(size: size)
+        let (xLines, _) = numberOfLines(size: size)
+        let xDelta = xLines/2
         let (xScaleFactor, _) = scaleFactor(size: size)
         var path = Path()
         path.move(to: CGPoint(x: 0, y: yOrigin))
@@ -72,15 +88,14 @@ struct GraphPaperView: View {
         context.stroke(path, with: .color(.black), lineWidth: 1)
         
         if showRule {
-            for i in 0...numberOfLines {
+            for i in 0...xLines {
                 var rule = Path()
                 let x = CGFloat(i) * xScaleFactor
                 
                 if showRuleLeyend {
                     context.draw(
-                        Text(String(i-10))
-                            .font(.footnote
-                                .weight(.light))
+                        Text(String(i-xDelta))
+                            .font(.system(size: 8))
                             .foregroundStyle(.gray),
                         at: CGPoint(x: x, y: yOrigin+15), anchor: .center)
                 }
@@ -94,22 +109,23 @@ struct GraphPaperView: View {
     
     func drawYAxis(context: GraphicsContext, size: CGSize) {
         let (xOrigin, _) = origin(size: size)
+        let (_, yLines) = numberOfLines(size: size)
         let (_, yScaleFactor) = scaleFactor(size: size)
+        let yDelta = yLines/2
         var path = Path()
         path.move(to: CGPoint(x: xOrigin, y: 0))
         path.addLine(to: CGPoint(x: xOrigin, y: size.height))
         context.stroke(path, with: .color(.black), lineWidth: 1)
         
         if showRule {
-            for i in 0...numberOfLines {
+            for i in 0...yLines {
                 var rule = Path()
                 let y = CGFloat(i) * yScaleFactor
                 
                 if showRuleLeyend {
                     context.draw(
-                        Text(String(i-10))
-                            .font(.footnote
-                                .weight(.light))
+                        Text(String(i-yDelta))
+                            .font(.system(size: 8))
                             .foregroundStyle(.gray),
                         at: CGPoint(x: xOrigin+15, y: y), anchor: .center)
                 }
@@ -123,5 +139,5 @@ struct GraphPaperView: View {
 }
 
 #Preview {
-    GraphPaperView(showRule: true, showRuleLeyend: false)
+    GraphPaperView(showRule: true, showRuleLeyend: true)
 }
